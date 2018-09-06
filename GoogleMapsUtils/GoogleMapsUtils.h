57db8fcf7596bf9c3677059b3ffa9d7625a51528
John DeLong <jdelong@michiganlabs.com>
2018-09-06 16:14:22 -0400

    Convert into a dynamic framework

---
+++ GoogleMapsUtils/GoogleMapsUtils.h
@@ -0,0 +1,36 @@
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleMapUtils/PublicHeader.h>
+
+#import <GoogleMapsUtils/GMUCluster.h>
+#import <GoogleMapsUtils/GMUClusterIconGenerator.h>
+#import <GoogleMapsUtils/GMUClusterItem.h>
+#import <GoogleMapsUtils/GMUClusterManager.h>
+#import <GoogleMapsUtils/GMUClusterRenderer.h>
+#import <GoogleMapsUtils/GMUDefaultClusterIconGenerator.h>
+#import <GoogleMapsUtils/GMUDefaultClusterRenderer.h>
+#import <GoogleMapsUtils/GMUFeature.h>
+#import <GoogleMapsUtils/GMUGeoJSONParser.h>
+#import <GoogleMapsUtils/GMUGeometry.h>
+#import <GoogleMapsUtils/GMUGeometryCollection.h>
+#import <GoogleMapsUtils/GMUGeometryContainer.h>
+#import <GoogleMapsUtils/GMUGeometryRenderer.h>
+#import <GoogleMapsUtils/GMUGradient.h>
+#import <GoogleMapsUtils/GMUGridBasedClusterAlgorithm.h>
+#import <GoogleMapsUtils/GMUGroundOverlay.h>
+#import <GoogleMapsUtils/GMUHeatmapTileLayer.h>
+#import <GoogleMapsUtils/GMUKMLParser.h>
+#import <GoogleMapsUtils/GMULineString.h>
+#import <GoogleMapsUtils/GMUMarkerClustering.h>
+#import <GoogleMapsUtils/GMUNonHierarchicalDistanceBasedAlgorithm.h>
+#import <GoogleMapsUtils/GMUPlacemark.h>
+#import <GoogleMapsUtils/GMUPoint.h>
+#import <GoogleMapsUtils/GMUPolygon.h>
+#import <GoogleMapsUtils/GMUSimpleClusterAlgorithm.h>
+#import <GoogleMapsUtils/GMUStaticCluster.h>
+#import <GoogleMapsUtils/GMUStyle.h>
+#import <GoogleMapsUtils/GMUWeightedLatLng.h>
+#import <GoogleMapsUtils/GMUWrappingDictionaryKey.h>
+#import <GoogleMapsUtils/GQTBounds.h>
+#import <GoogleMapsUtils/GQTPoint.h>
+#import <GoogleMapsUtils/GQTPointQuadTree.h>
+#import <GoogleMapsUtils/GQTPointQuadTreeChild.h>
+#import <GoogleMapsUtils/GQTPointQuadTreeItem.h>
